93573a51012667a3c7875333f02c63a9499d4e93
Olesia Pankova <pankovaolesya77@yandex.ru>
2024-12-05 19:52:21 +0400

    edits 1.2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,28 +1,11 @@
-from gendiff.data.build_diff import build_diff
-from gendiff.formatters.json_format import json_format
-from gendiff.formatters.plain import plain
-from gendiff.formatters.stylish import stylish
-from gendiff.scripts.parser import get_extension, parse_file
-
-FORMAT_NAMES = {
-    'stylish': stylish,
-    'plain': plain,
-    'json': json_format
-}
-
-
-def generate_diff(file1, file2, format_name='stylish'):
-    data1 = parse_file(file1, get_extension(file1))
-    data2 = parse_file(file2, get_extension(file2))
-    diff = build_diff(data1, data2)
-    if isinstance(format_name, str):
-        format_name = FORMAT_NAMES[format_name]
-    diff = format_name(diff)
-    return diff
+from gendiff.cli import cli_args
+from gendiff.engine import generate_diff
 
 
 def main():
-    generate_diff()
+    file1, file2, format_name = cli_args()
+    diff = generate_diff(file1, file2, format_name)
+    print(diff)
 
 
 if __name__ == '__main__':
