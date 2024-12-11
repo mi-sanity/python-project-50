343176bec8e8e07612ef0e006936467df588f4e0
Olesia Pankova <pankovaolesya77@yandex.ru>
2024-12-11 20:10:50 +0400

    edits for Check 3 v.2

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -3,49 +3,54 @@ DEFAULT_INDENT = 4
 
 def stylish(diff, depth=0):
     tree = []
+    indent = " " * (depth * DEFAULT_INDENT)
 
     for key in diff:
         item = diff[key]
-        status = item["status"]
-        val = item["value"]
-        value = to_str(val, depth)
-        indent = " " * (depth * DEFAULT_INDENT)
-        prefix = "    "
-        prefix_plus = "  + "
-        prefix_minus = "  - "
+        stylish_formatted = stylish_format(key, item, indent, depth)
+        tree.append(stylish_formatted)
+    tree = "\n".join(tree)
+    return f"{{\n{tree}\n{indent}}}"
 
-        if status == "unchanged":
-            tree.append(f"{indent}{prefix}{key}: {value}")
-        elif status == "added":
-            tree.append(f"{indent}{prefix_plus}{key}: {value}")
-        elif status == "removed":
-            tree.append(f"{indent}{prefix_minus}{key}: {value}")
-        elif status == "changed":
-            old, new = val
-            old_value = to_str(old, depth)
-            new_value = to_str(new, depth)
-            tree.append(f"{indent}{prefix_minus}{key}: {old_value}")
-            tree.append(f"{indent}{prefix_plus}{key}: {new_value}")
-        elif status == "nested":
-            tree.append(
-                f"{indent}{prefix}{key}: {stylish(val, depth + 1)}"
-            )
-    format = "\n".join(tree)
-    return f"{{\n{format}\n{indent}}}"
+
+def stylish_format(key, item, indent, depth):
+    status = item["status"]
+    val = item["value"]
+    value = to_str(val, depth)
+    prefix = "    "
+    prefix_plus = "  + "
+    prefix_minus = "  - "
+
+    if status == "unchanged":
+        return f"{indent}{prefix}{key}: {value}"
+    elif status == "added":
+        return f"{indent}{prefix_plus}{key}: {value}"
+    elif status == "removed":
+        return f"{indent}{prefix_minus}{key}: {value}"
+    elif status == "changed":
+        old, new = val
+        old_value = to_str(old, depth)
+        new_value = to_str(new, depth)
+        return (
+            f"{indent}{prefix_minus}{key}: {old_value}"
+            f"\n{indent}{prefix_plus}{key}: {new_value}"
+        )
+    elif status == "nested":
+        return f"{indent}{prefix}{key}: {stylish(val, depth + 1)}"
 
 
 def to_str(value, depth, indent=0):
     if isinstance(value, dict):
-        format_value = []
+        lines = []
         nested_indent1 = " " * ((depth + 1) * DEFAULT_INDENT)
         nested_indent2 = " " * ((depth + 2) * DEFAULT_INDENT)
         for key, val in value.items():
-            format_value.append(
+            lines.append(
                 f"{nested_indent2}{key}: "
                 f"{to_str(val, depth + 1, DEFAULT_INDENT)}"
             )
-        formatted_str = "\n".join(format_value)
-        return f"{{\n{formatted_str}\n{nested_indent1}}}"
+        lines = "\n".join(lines)
+        return f"{{\n{lines}\n{nested_indent1}}}"
     elif isinstance(value, bool):
         return str(value).lower()
     elif value is None:
